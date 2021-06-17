7f0eb285cbbe1a38f24464dbc933cd1a006721ac
andrew.rosario <andrew.rosario@shift.com.br>
2021-06-17 20:16:40 -0300

    test: use HasherService on LastFmRequestSignatureService

--- projects/last-fm/src/lib/services/last-fm-request-signature/last-fm-request-signature.service.spec.ts
+++ projects/last-fm/src/lib/services/last-fm-request-signature/last-fm-request-signature.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HasherService } from 'projects/catalog-fm-utils/src/public-api';
 import { MOCK_LAST_FM_HTTP_PARAMS } from '../../mocks/last-fm-http-params.mock';
 import { MOCK_LAST_FM_KEY } from '../../mocks/last-fm-key.mock';
 import { LAST_FM_KEY } from '../../tokens/last-fm-key.token';
@@ -7,15 +8,29 @@ import { LastFmRequestSignatureService } from './last-fm-request-signature.servi
 
 describe('LastFmRequestSignatureService', () => {
   let service: LastFmRequestSignatureService;
+  const hashMockValue = 'hashed';
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [{ provide: LAST_FM_KEY, useValue: MOCK_LAST_FM_KEY }],
+      providers: [
+        { provide: HasherService, useValue: { hash: () => hashMockValue } },
+        { provide: LAST_FM_KEY, useValue: MOCK_LAST_FM_KEY },
+      ],
     });
     service = TestBed.inject(LastFmRequestSignatureService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should make signature with correct values', () => {
+    const signature = service.makeSignature(MOCK_LAST_FM_HTTP_PARAMS);
+    expect(signature).toEqual({
+      api_key: MOCK_LAST_FM_KEY.apiPublicKey,
+      api_sig: hashMockValue,
+      key: MOCK_LAST_FM_HTTP_PARAMS.data?.key,
+      method: MOCK_LAST_FM_HTTP_PARAMS.method,
+    });
   });
 });

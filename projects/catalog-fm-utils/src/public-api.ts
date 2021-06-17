4a80a5f68661d06d2a92b78f5140bd45882c952a
andrew.rosario <andrew.rosario@shift.com.br>
2021-06-17 19:44:20 -0300

    refactor: remove unused files

--- projects/catalog-fm-utils/src/public-api.ts
+++ projects/catalog-fm-utils/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of catalog-fm-utils
  */
 
-export * from './lib/catalog-fm-utils.service';
-export * from './lib/catalog-fm-utils.component';
+export * from './lib/services/hasher/hasher.service';
 export * from './lib/catalog-fm-utils.module';

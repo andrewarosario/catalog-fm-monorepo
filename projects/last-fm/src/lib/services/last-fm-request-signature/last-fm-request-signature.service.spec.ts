1676e864e865613475e977d5df46b053dd294b7d
andrew.rosario <andrew.rosario@shift.com.br>
2021-06-18 09:00:30 -0300

    test: should call HasherService.hash on make signature

--- projects/last-fm/src/lib/services/last-fm-request-signature/last-fm-request-signature.service.spec.ts
+++ projects/last-fm/src/lib/services/last-fm-request-signature/last-fm-request-signature.service.spec.ts
@@ -8,20 +8,34 @@ import { LastFmRequestSignatureService } from './last-fm-request-signature.servi
 
 describe('LastFmRequestSignatureService', () => {
   let service: LastFmRequestSignatureService;
+  let hasherServiceSpy: jasmine.SpyObj<HasherService>;
   const hashMockValue = 'hashed';
 
+  function makeHasherServiceSpy() {
+    hasherServiceSpy = TestBed.inject(HasherService) as jasmine.SpyObj<HasherService>;
+    hasherServiceSpy.hash.and.returnValue(hashMockValue);
+  }
+
   beforeEach(() => {
+    const hashSpy = jasmine.createSpyObj<HasherService>('HasherService', ['hash']);
+
     TestBed.configureTestingModule({
       providers: [
-        { provide: HasherService, useValue: { hash: () => hashMockValue } },
+        { provide: HasherService, useValue: hashSpy },
         { provide: LAST_FM_KEY, useValue: MOCK_LAST_FM_KEY },
       ],
     });
     service = TestBed.inject(LastFmRequestSignatureService);
+    makeHasherServiceSpy();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call HasherService.hash on make signature', () => {
+    service.makeSignature(MOCK_LAST_FM_HTTP_PARAMS);
+    expect(hasherServiceSpy.hash).toHaveBeenCalled();
   });
 
   it('should make signature with correct values', () => {

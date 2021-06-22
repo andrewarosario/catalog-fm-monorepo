1bbd0e61417d025c70f043672f0af8fb1494bb99
andrew.rosario <andrew.rosario@shift.com.br>
2021-06-22 11:59:36 -0300

    refactor: move HasherService creation to a factory method

--- projects/catalog-fm-utils/src/lib/services/hasher/hasher.service.spec.ts
+++ projects/catalog-fm-utils/src/lib/services/hasher/hasher.service.spec.ts
@@ -1,28 +1,27 @@
-import { TestBed } from '@angular/core/testing';
 import { HasherService } from './hasher.service';
 
 import { Md5 } from 'ts-md5/dist/md5';
 
+const makeSut = () => {
+  const service = new HasherService();
+  const hashSpy = spyOn<any>(Md5, 'hashStr').and.returnValue('hashed value');
+  return { service, hashSpy };
+};
+
 describe('HasherService', () => {
-  let service: HasherService;
-  let hashSpy: jasmine.Spy;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(HasherService);
-    hashSpy = spyOn<any>(Md5, 'hashStr').and.returnValue('hashed value');
-  });
-
   it('should be created', () => {
+    const service = new HasherService();
     expect(service).toBeTruthy();
   });
 
   it('should call Md5.hashStr with correct value', () => {
+    const { service, hashSpy } = makeSut();
     service.hash('value');
     expect(hashSpy).toHaveBeenCalledWith('value');
   });
 
   it('should return the correct value on hash', () => {
+    const { service } = makeSut();
     const value = service.hash('value');
     expect(value).toBe('hashed value');
   });

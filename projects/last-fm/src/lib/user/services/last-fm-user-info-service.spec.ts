194482cce77d8086cd0e6a2ed797eb951ec4dbfc
andrew.rosario <andrew.rosario@shift.com.br>
2021-06-23 11:30:28 -0300

    refactor: rename LastFmUserService to LastFmUserInfoService

---
+++ projects/last-fm/src/lib/user/services/last-fm-user-info-service.spec.ts
@@ -0,0 +1,8 @@
+import { LastFmUserInfoService } from './last-fm-user-info.service';
+
+describe('LastFmUserInfoService', () => {
+  it('should be created', () => {
+    const service = new LastFmUserInfoService();
+    expect(service).toBeTruthy();
+  });
+});
--- projects/last-fm/src/lib/user/services/last-fm-user-service.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LastFmUserService } from './last-fm-user.service';
-
-describe('LastFmUserService', () => {
-  it('should be created', () => {
-    const service = new LastFmUserService();
-    expect(service).toBeTruthy();
-  });
-});

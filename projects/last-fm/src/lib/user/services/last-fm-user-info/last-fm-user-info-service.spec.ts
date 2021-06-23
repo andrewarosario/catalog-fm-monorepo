2bd566f47f5c2d008838855ffe1075eb6a1dc420
andrew.rosario <andrew.rosario@shift.com.br>
2021-06-23 12:00:41 -0300

    test: should LastFmUserInfoService call LastFmHttp.get with correct values

--- projects/last-fm/src/lib/user/services/last-fm-user-info/last-fm-user-info-service.spec.ts
+++ projects/last-fm/src/lib/user/services/last-fm-user-info/last-fm-user-info-service.spec.ts
@@ -1,8 +1,21 @@
+import { LastFmMethod } from '@/api/enums/last-fm-method';
+import { makeLastFmHttpSpy } from '@/api/services/last-fm-http/last-fm-http.service.mock';
 import { LastFmUserInfoService } from './last-fm-user-info.service';
 
 describe('LastFmUserInfoService', () => {
   it('should be created', () => {
-    const service = new LastFmUserInfoService();
+    const lastFmHttp = makeLastFmHttpSpy({});
+    const service = new LastFmUserInfoService(lastFmHttp);
     expect(service).toBeTruthy();
+  });
+
+  it('should call LastFmHttp.get with correct values', () => {
+    const lastFmHttp = makeLastFmHttpSpy({});
+    const service = new LastFmUserInfoService(lastFmHttp);
+    service.getInfo('user').subscribe();
+    expect(lastFmHttp.get).toHaveBeenCalledOnceWith({
+      method: LastFmMethod.UserGetInfo,
+      data: { user: 'user' },
+    });
   });
 });

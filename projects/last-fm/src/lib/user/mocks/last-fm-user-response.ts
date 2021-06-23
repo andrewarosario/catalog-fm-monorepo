cc3ccc9e84928ea5a865e77cd6930401b746a42b
andrew.rosario <andrew.rosario@shift.com.br>
2021-06-23 12:07:12 -0300

    test: ensure LastFmUserInfoService returns the right data from getInfo

---
+++ projects/last-fm/src/lib/user/mocks/last-fm-user-response.ts
@@ -0,0 +1,28 @@
+import { LastFmUserResponse } from '../models/last-fm-user-response';
+
+export const MOCK_LAST_FM_USER_RESPONSE: LastFmUserResponse = {
+  user: {
+    age: '20',
+    country: 'brazil',
+    gender: 'f',
+    name: 'maria',
+    playcount: '2550',
+    playlists: '',
+    realname: 'Maria',
+    subscriber: '0',
+    url: 'https://last.fm/user/maria',
+    type: 'user',
+    bootstrap: '',
+    id: '954',
+    registered: {
+      unixtime: '000',
+      '#text': '01 Jan 2022',
+    },
+    image: [
+      { '#text': 'small', size: 'small' },
+      { '#text': 'medium', size: 'medium' },
+      { '#text': 'large', size: 'large' },
+      { '#text': 'extralarge', size: 'extralarge' },
+    ],
+  },
+};
--- projects/last-fm/src/lib/user/services/last-fm-user-info/last-fm-user-info-service.spec.ts
+++ projects/last-fm/src/lib/user/services/last-fm-user-info/last-fm-user-info-service.spec.ts
@@ -1,9 +1,10 @@
 import { LastFmMethod } from '@/api/enums/last-fm-method';
 import { makeLastFmHttpSpy } from '@/api/services/last-fm-http/last-fm-http.service.mock';
+import { MOCK_LAST_FM_USER_RESPONSE } from '@/user/mocks/last-fm-user-response';
 import { LastFmUserInfoService } from './last-fm-user-info.service';
 
 const makeSut = () => {
-  const lastFmHttp = makeLastFmHttpSpy({});
+  const lastFmHttp = makeLastFmHttpSpy(MOCK_LAST_FM_USER_RESPONSE);
   const service = new LastFmUserInfoService(lastFmHttp);
   return { service, lastFmHttp };
 };
@@ -20,6 +21,13 @@ describe('LastFmUserInfoService', () => {
     expect(lastFmHttp.get).toHaveBeenCalledOnceWith({
       method: LastFmMethod.UserGetInfo,
       data: { user: 'user' },
+    });
+  });
+
+  it('should return the right data from getInfo', () => {
+    const { service } = makeSut();
+    service.getInfo('user').subscribe((res) => {
+      expect(res).toEqual(MOCK_LAST_FM_USER_RESPONSE);
     });
   });
 });

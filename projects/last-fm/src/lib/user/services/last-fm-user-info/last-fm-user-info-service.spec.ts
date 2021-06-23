1e83461c0c452de6cabe83118f6205dfa90acf19
andrew.rosario <andrew.rosario@shift.com.br>
2021-06-23 12:02:28 -0300

    refactor: move LastFmUserInfoService test creation to a factory method

--- projects/last-fm/src/lib/user/services/last-fm-user-info/last-fm-user-info-service.spec.ts
+++ projects/last-fm/src/lib/user/services/last-fm-user-info/last-fm-user-info-service.spec.ts
@@ -2,16 +2,20 @@ import { LastFmMethod } from '@/api/enums/last-fm-method';
 import { makeLastFmHttpSpy } from '@/api/services/last-fm-http/last-fm-http.service.mock';
 import { LastFmUserInfoService } from './last-fm-user-info.service';
 
+const makeSut = () => {
+  const lastFmHttp = makeLastFmHttpSpy({});
+  const service = new LastFmUserInfoService(lastFmHttp);
+  return { service, lastFmHttp };
+};
+
 describe('LastFmUserInfoService', () => {
   it('should be created', () => {
-    const lastFmHttp = makeLastFmHttpSpy({});
-    const service = new LastFmUserInfoService(lastFmHttp);
+    const { service } = makeSut();
     expect(service).toBeTruthy();
   });
 
   it('should call LastFmHttp.get with correct values', () => {
-    const lastFmHttp = makeLastFmHttpSpy({});
-    const service = new LastFmUserInfoService(lastFmHttp);
+    const { service, lastFmHttp } = makeSut();
     service.getInfo('user').subscribe();
     expect(lastFmHttp.get).toHaveBeenCalledOnceWith({
       method: LastFmMethod.UserGetInfo,

194b9a866d369761d90c08d4fc20e38c7d57acfe
andrew.rosario <andrew.rosario@shift.com.br>
2021-06-23 11:27:35 -0300

    test: create LastFmUserService

---
+++ projects/last-fm/src/lib/user/services/last-fm-user-service.spec.ts
@@ -0,0 +1,8 @@
+import { LastFmUserService } from './last-fm-user.service';
+
+describe('LastFmUserService', () => {
+  it('should be created', () => {
+    const service = new LastFmUserService();
+    expect(service).toBeTruthy();
+  });
+});

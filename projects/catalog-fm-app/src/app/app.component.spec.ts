65e97486fd60358501f3c660e8c6bf53feaa068e
Andrew Rosário <andrew.arosario@gmail.com>
2021-06-27 15:48:55 -0300

    test: app title

--- projects/catalog-fm-app/src/app/app.component.spec.ts
+++ projects/catalog-fm-app/src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'catalog-fm-app'`, () => {
+  it(`should have as title 'Catalog.fm'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('catalog-fm-app');
+    expect(app.title).toEqual('Catalog.fm');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('catalog-fm-app app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Catalog.fm');
   });
 });

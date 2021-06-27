e0d9ea47dd97295c76b82731bc4ecfcb9f1ed753
Andrew Rosário <andrew.arosario@gmail.com>
2021-06-27 17:22:09 -0300

    feat: create providers for LastFmModule

--- projects/last-fm/src/lib/last-fm.module.ts
+++ projects/last-fm/src/lib/last-fm.module.ts
@@ -1,7 +1,21 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { LastFmKey } from './api/models/last-fm-key';
+import { LAST_FM_KEY } from './api/tokens/last-fm-key.token';
 
 @NgModule({
   imports: [HttpClientModule],
 })
-export class LastFmModule {}
+export class LastFmModule {
+  static forRoot(lastFmKey: LastFmKey): ModuleWithProviders<LastFmModule> {
+    return {
+      ngModule: LastFmModule,
+      providers: [
+        {
+          provide: LAST_FM_KEY,
+          useValue: lastFmKey,
+        },
+      ],
+    };
+  }
+}

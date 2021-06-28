a7910563446f9c893493e238f3638ec6c2ba96c0
andrew.rosario <andrew.rosario@shift.com.br>
2021-06-28 09:52:38 -0300

    feat: hide last.fm key

--- projects/catalog-fm-app/src/environments/environment.prod.ts
+++ projects/catalog-fm-app/src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
+import { CONFIG } from '../../../../config';
+
 export const environment = {
   production: true,
   lastFmKey: {
     apiPublicKey: '86c10d742e10e0fca5fee5e69612c848',
-    apiSecretKey: '216885feaf60bbe7104e6e1dcb3f675e',
+    apiSecretKey: CONFIG.LAST_FM_SECRET_PROD,
   },
 };
--- projects/catalog-fm-app/src/environments/environment.ts
+++ projects/catalog-fm-app/src/environments/environment.ts
@@ -1,7 +1,9 @@
+import { CONFIG } from '../../../../config';
+
 export const environment = {
   production: false,
   lastFmKey: {
     apiPublicKey: 'ace63c61153c7d8bc456af3f054b8df4',
-    apiSecretKey: '237fa9131fdc42dd9a2d0c02ef36937f',
+    apiSecretKey: CONFIG.LAST_FM_SECRET_DEV,
   },
 };

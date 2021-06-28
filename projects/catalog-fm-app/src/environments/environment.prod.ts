521347a7a0934fe044d7bdc5c75340192095904a
Andrew Rosário <andrew.arosario@gmail.com>
2021-06-27 23:17:05 -0300

    feat: apply last.fm key on environment

--- projects/catalog-fm-app/src/environments/environment.prod.ts
+++ projects/catalog-fm-app/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  lastFmKey: {
+    apiPublicKey: '86c10d742e10e0fca5fee5e69612c848',
+    apiSecretKey: '216885feaf60bbe7104e6e1dcb3f675e',
+  },
 };
--- projects/catalog-fm-app/src/environments/environment.ts
+++ projects/catalog-fm-app/src/environments/environment.ts
@@ -1,16 +1,7 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  lastFmKey: {
+    apiPublicKey: 'ace63c61153c7d8bc456af3f054b8df4',
+    apiSecretKey: '237fa9131fdc42dd9a2d0c02ef36937f',
+  },
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.

19433c2d994eca23c4d39822d39682e68ddde368
andrew.rosario <andrew.rosario@shift.com.br>
2021-06-29 16:54:45 -0300

    test: ensure AuthPageComponent calls LastFmAuthRedirectService.redirect with correct values when call lastFmRedirect

--- projects/catalog-fm-app/src/app/auth/pages/auth-page/auth-page.component.spec.ts
+++ projects/catalog-fm-app/src/app/auth/pages/auth-page/auth-page.component.spec.ts
@@ -1,25 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LastFmAuthRedirectService } from 'last-fm';
 
 import { AuthPageComponent } from './auth-page.component';
 
+const makeLastFmAuthRedirectService = (): jasmine.SpyObj<LastFmAuthRedirectService> => {
+  const serviceSpy = jasmine.createSpyObj<LastFmAuthRedirectService>('LastFmAuthRedirectService', [
+    'redirect',
+  ]);
+  return serviceSpy;
+};
+
+const makeSut = () => {
+  const lastFmAuthRedirectServiceSpy = makeLastFmAuthRedirectService();
+  const component = new AuthPageComponent(lastFmAuthRedirectServiceSpy);
+  return { component, lastFmAuthRedirectServiceSpy };
+};
+
 describe('AuthPageComponent', () => {
-  let component: AuthPageComponent;
-  let fixture: ComponentFixture<AuthPageComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AuthPageComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AuthPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
   it('should create', () => {
+    const { component } = makeSut();
     expect(component).toBeTruthy();
+  });
+
+  it('should call LastFmAuthRedirectService.redirect with correct values when call lastFmRedirect', () => {
+    const { component, lastFmAuthRedirectServiceSpy } = makeSut();
+    component.lastFmRedirect();
+    expect(lastFmAuthRedirectServiceSpy.redirect).toHaveBeenCalledOnceWith('callback');
   });
 });

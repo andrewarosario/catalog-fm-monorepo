84e4494b7906ee57cf81cda71ecb2e892dcde012
andrew.rosario <andrew.rosario@shift.com.br>
2021-06-30 10:44:39 -0300

    feat: create AuthUserStore

---
+++ projects/catalog-fm-app/src/app/core/auth/store/auth-user.store.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { AuthLastFmSession, AuthUser } from '../models/auth-user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthUserStore {
+  private authUserSubject$ = new BehaviorSubject<AuthUser>(null);
+  authUser$ = this.authUserSubject$.asObservable();
+
+  setLastFmSession(lastFmSession: AuthLastFmSession): Observable<AuthUser> {
+    this.authUserSubject$.next({ lastFmSession });
+    return this.authUser$;
+  }
+}

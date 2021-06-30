ae258fc11df3ef96d4468933c4ace5f3b2eed5b0
andrew.rosario <andrew.rosario@shift.com.br>
2021-06-30 08:30:08 -0300

    feat: use LocalStorageService as default on StorageService

--- projects/catalog-fm-utils/src/lib/services/storage/storage.service.ts
+++ projects/catalog-fm-utils/src/lib/services/storage/storage.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
+import { LocalStorageService } from './local-storage/local-storage.service';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({
+  providedIn: 'root',
+  useClass: LocalStorageService,
+})
 export abstract class StorageService {
   abstract getItem<T>(key: string): Promise<T>;
   abstract setItem<T>(key: string, value: any): Promise<void>;

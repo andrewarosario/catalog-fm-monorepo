874f76a28dbdfa56294509fec96aaafaf51701fc
andrewarosario <andrew.arosario@gmail.com>
2021-07-01 10:32:31 -0300

    feat: allow promises and booleans in canActivateComposite params

--- projects/catalog-fm-utils/src/lib/utils/can-activate-composite/can-activate-composite.ts
+++ projects/catalog-fm-utils/src/lib/utils/can-activate-composite/can-activate-composite.ts
@@ -1,10 +1,14 @@
-import { Observable, concat } from 'rxjs';
+import { Observable, concat, of } from 'rxjs';
 import { takeWhile, toArray, map } from 'rxjs/operators';
 
 export const canActivateComposite = (
-  ...canActivate: Observable<boolean>[]
+  ...canActivate: Array<Observable<boolean> | Promise<boolean> | boolean>
 ): Observable<boolean> => {
-  return concat(...canActivate).pipe(
+  const canActivateAsync = canActivate.map((value) =>
+    typeof value === 'boolean' ? of(value) : value
+  );
+
+  return concat(...canActivateAsync).pipe(
     takeWhile(Boolean),
     toArray(),
     map((arr) => arr.length === canActivate.length)

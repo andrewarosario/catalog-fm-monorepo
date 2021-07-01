973710578bdfac289a262b5b1fbce2738806ca6e
andrewarosario <andrew.arosario@gmail.com>
2021-07-01 19:49:05 -0300

    feat: create LastFmModule

---
+++ projects/catalog-fm-app/src/app/last-fm/last-fm.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LastFmRoutingModule } from './last-fm-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LastFmRoutingModule
+  ]
+})
+export class LastFmModule { }

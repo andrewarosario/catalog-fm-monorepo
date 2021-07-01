15c9eee821209541a4d06c0a67402614df3b9dec
andrewarosario <andrew.arosario@gmail.com>
2021-07-01 10:32:52 -0300

    test: allow promises and booleans in canActivateComposite params

--- projects/catalog-fm-utils/src/lib/utils/can-activate-composite/can-activate-composite.spec.ts
+++ projects/catalog-fm-utils/src/lib/utils/can-activate-composite/can-activate-composite.spec.ts
@@ -1,9 +1,10 @@
+import { fakeAsync, tick } from '@angular/core/testing';
 import { of } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { canActivateComposite } from './can-activate-composite';
 
 describe('canActivateComposite', () => {
-  it('should return true when all params returns true', () => {
+  it('should return true when all params return true', () => {
     const param1 = of(true);
     const param2 = of(true);
     const param3 = of(true);
@@ -13,7 +14,7 @@ describe('canActivateComposite', () => {
     });
   });
 
-  it('should return false when any params returns false', () => {
+  it('should return false when any params return false', () => {
     const param1 = of(true);
     const param2 = of(false);
     const param3 = of(true);
@@ -23,7 +24,7 @@ describe('canActivateComposite', () => {
     });
   });
 
-  it('should return false when all params returns false', () => {
+  it('should return false when all params return false', () => {
     const param1 = of(false);
     const param2 = of(false);
     const param3 = of(false);
@@ -33,7 +34,76 @@ describe('canActivateComposite', () => {
     });
   });
 
-  it('should call all functions when all params returns true', () => {
+  it('should return true when all params return true, including boolean param', () => {
+    const param1 = of(true);
+    const param2 = true;
+    const param3 = of(true);
+    const param4 = of(true);
+
+    canActivateComposite(param1, param2, param3, param4).subscribe((val) => {
+      expect(val).toBe(true);
+    });
+  });
+
+  it('should return false when boolean params return false', () => {
+    const param1 = of(true);
+    const param2 = of(true);
+    const param3 = of(true);
+    const param4 = false;
+
+    canActivateComposite(param1, param2, param3, param4).subscribe((val) => {
+      expect(val).toBe(false);
+    });
+  });
+
+  it('should return false when all params return false, including boolean param', () => {
+    const param1 = false;
+    const param2 = of(false);
+    const param3 = of(false);
+    const param4 = of(false);
+
+    canActivateComposite(param1, param2, param3, param4).subscribe((val) => {
+      expect(val).toBe(false);
+    });
+  });
+
+  it('should return true when all params return true, including promise param', fakeAsync(() => {
+    const param1 = of(true);
+    const param2 = Promise.resolve(true);
+    const param3 = of(true);
+    const param4 = of(true);
+
+    canActivateComposite(param1, param2, param3, param4).subscribe((val) => {
+      tick();
+      expect(val).toBe(true);
+    });
+  }));
+
+  it('should return false when promise params return false', fakeAsync(() => {
+    const param1 = of(true);
+    const param2 = of(true);
+    const param3 = of(true);
+    const param4 = Promise.resolve(false);
+
+    canActivateComposite(param1, param2, param3, param4).subscribe((val) => {
+      tick();
+      expect(val).toBe(false);
+    });
+  }));
+
+  it('should return false when all params return false, including promise param', fakeAsync(() => {
+    const param1 = Promise.resolve(false);
+    const param2 = of(false);
+    const param3 = of(false);
+    const param4 = of(false);
+
+    canActivateComposite(param1, param2, param3, param4).subscribe((val) => {
+      tick();
+      expect(val).toBe(false);
+    });
+  }));
+
+  it('should call all functions when all params return true', () => {
     let count = 0;
     const param1 = of(true).pipe(tap(() => count++));
     const param2 = of(true).pipe(tap(() => count++));
@@ -44,7 +114,7 @@ describe('canActivateComposite', () => {
     });
   });
 
-  it('should call function two times when second param returns false', () => {
+  it('should call function two times when second param return false', () => {
     let count = 0;
     const param1 = of(true).pipe(tap(() => count++));
     const param2 = of(false).pipe(tap(() => count++));
@@ -55,7 +125,7 @@ describe('canActivateComposite', () => {
     });
   });
 
-  it('should call function one time when all params returns false', () => {
+  it('should call function one time when all params return false', () => {
     let count = 0;
     const param1 = of(false).pipe(tap(() => count++));
     const param2 = of(false).pipe(tap(() => count++));

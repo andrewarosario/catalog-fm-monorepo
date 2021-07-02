fde24d930dc2587cfb6889958481d8c3b67e1a8c
Andrew Rosário <andrew.arosario@gmail.com>
2021-07-01 21:21:53 -0300

    feat: create BulkScrobbleModule

---
+++ projects/catalog-fm-app/src/app/last-fm/bulk-scrobble/bulk-scrobble.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BulkScrobbleRoutingModule } from './bulk-scrobble-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BulkScrobbleRoutingModule
+  ]
+})
+export class BulkScrobbleModule { }

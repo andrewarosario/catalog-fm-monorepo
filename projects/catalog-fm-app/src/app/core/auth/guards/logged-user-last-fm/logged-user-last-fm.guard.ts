93a7454107251384508cb3529fd26a645a2e14a7
Andrew Rosário <andrew.arosario@gmail.com>
2021-07-02 00:10:21 -0300

    feat: create LoggedUserLastFmGuard

---
+++ projects/catalog-fm-app/src/app/core/auth/guards/logged-user-last-fm/logged-user-last-fm.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+
+import { LoggedUserLastFmService } from '../../services/logged-user-last-fm/logged-user-last-fm.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoggedUserLastFmGuard implements CanActivate {
+  constructor(private loggedUserLastFmService: LoggedUserLastFmService, private router: Router) {}
+  async canActivate(): Promise<boolean> {
+    const lastFmSession = await this.loggedUserLastFmService.getLoggedUser();
+    if (!lastFmSession) {
+      this.router.navigateByUrl('/auth');
+      return false;
+    }
+    return true;
+  }
+}

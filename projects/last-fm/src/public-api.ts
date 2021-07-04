7ce916612ec951755794e71afb5fa9093d5818f1
Andrew Rosário <andrew.arosario@gmail.com>
2021-07-04 15:32:49 -0300

    feat: export last.fm track files

--- projects/last-fm/src/public-api.ts
+++ projects/last-fm/src/public-api.ts
@@ -8,4 +8,6 @@ export * from './lib/api/services/last-fm-http/last-fm-http.service';
 export * from './lib/auth/services/last-fm-auth/last-fm-auth.service';
 export * from './lib/auth/services/last-fm-auth-redirect/last-fm-auth-redirect.service';
 export * from './lib/auth/mocks/last-fm-auth-response.mock';
+export * from './lib/track/services/last-fm-track-scrobble/last-fm-track-scrobble.service';
+export * from './lib/track/models/last-fm-simple-track';
 export * from './lib/last-fm.module';

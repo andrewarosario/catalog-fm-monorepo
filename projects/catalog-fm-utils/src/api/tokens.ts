1d2de4b18cb99e6b4c50d5ab5ae82dd21a7d49f0
Andrew Rosário <andrew.arosario@gmail.com>
2021-07-06 10:22:49 -0300

    refactor: remove unix timestamp token

--- projects/catalog-fm-utils/src/api/tokens.ts
+++ projects/catalog-fm-utils/src/api/tokens.ts
@@ -1,4 +1,3 @@
 export * from '../lib/tokens/window/window.token';
 export * from '../lib/tokens/is-offline/is-offline.token';
 export * from '../lib/tokens/window/window.mock';
-export * from '../lib/tokens/unix-timestamp/unix-timestamp.token';
--- projects/catalog-fm-utils/src/lib/tokens/unix-timestamp/unix-timestamp.token.ts
+++
@@ -1,7 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-export const unixTimestamp = () => Math.floor(+new Date() / 1000);
-
-export const UNIX_TIMESTAMP = new InjectionToken<number>('Returns unix timestamp', {
-  factory: unixTimestamp,
-});

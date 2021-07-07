b6c29e0e3ff1892390e9cb24874806f2a7a6f05a
andrewarosario <andrew.arosario@gmail.com>
2021-07-07 15:47:34 -0300

    refactor: move last.fm track text scrobble mock to a separate file

---
+++ projects/catalog-fm-app/src/app/last-fm/bulk-scrobble/mocks/last-fm-text-scrobble.mock.ts
@@ -0,0 +1,5 @@
+export const mockLastFmTextScrobble = () => {
+  return `Artist 1-Track 1-Album 1
+Artist 2-Track 2-Album 2
+Artist 3-Track 3`;
+};
--- projects/catalog-fm-app/src/app/last-fm/bulk-scrobble/services/bulk-scrobble-converter/bulk-scrobble-converter.service.spec.ts
+++ projects/catalog-fm-app/src/app/last-fm/bulk-scrobble/services/bulk-scrobble-converter/bulk-scrobble-converter.service.spec.ts
@@ -1,11 +1,6 @@
 import { mockLastFmSimpleTrackScrobble } from '../../mocks/last-fm-simple-track-scrobble.mock';
+import { mockLastFmTextScrobble } from '../../mocks/last-fm-text-scrobble.mock';
 import { BulkScrobbleConverter } from './bulk-scrobble-converter.service';
-
-const mockText = () => {
-  return `Artist 1-Track 1-Album 1
-Artist 2-Track 2-Album 2
-Artist 3-Track 3`;
-};
 
 const makeSut = () => new BulkScrobbleConverter();
 
@@ -17,7 +12,7 @@ describe('BulkScrobbleConverter', () => {
 
   it('should convert text to last.fm track', () => {
     const service = makeSut();
-    const tracks = service.convert(mockText());
+    const tracks = service.convert(mockLastFmTextScrobble());
     expect(tracks).toEqual(mockLastFmSimpleTrackScrobble());
   });
 });

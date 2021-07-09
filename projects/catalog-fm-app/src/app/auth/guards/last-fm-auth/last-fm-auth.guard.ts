2c70ed96238ed8b50703ed438aea417a0efe8bf2
andrewarosario <andrew.arosario@gmail.com>
2021-07-09 16:59:18 -0300

    feat: ensure LastFmAuthGuard navigates to an url after authenticate

--- projects/catalog-fm-app/src/app/auth/guards/last-fm-auth/last-fm-auth.guard.ts
+++ projects/catalog-fm-app/src/app/auth/guards/last-fm-auth/last-fm-auth.guard.ts
@@ -1,22 +1,21 @@
 import { getAuthToken } from '@/auth/utils/get-auth-token';
 import { AuthUserLastFmService } from '@/core/auth/services/auth-user-last-fm/auth-user-last-fm.service';
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LastFmAuthGuard implements CanActivate {
-  constructor(private authUserLastFmService: AuthUserLastFmService) {}
+  constructor(private authUserLastFmService: AuthUserLastFmService, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
     const token = getAuthToken(route);
     return this.authUserLastFmService.authenticate(token).pipe(
-      map((authUser) => {
-        return !!authUser;
-      })
+      map((authUser) => !!authUser),
+      tap((isAuthenticated) => this.router.navigateByUrl(isAuthenticated ? '/last-fm' : '/auth'))
     );
   }
 }

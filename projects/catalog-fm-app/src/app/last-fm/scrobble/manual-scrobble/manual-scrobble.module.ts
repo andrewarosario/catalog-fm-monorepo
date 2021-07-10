126aaf7094cab2bde21f58bb3b474b447ceab571
Andrew Rosário <andrew.arosario@gmail.com>
2021-07-09 22:17:21 -0300

    feat: create ManualScrobbleModule

---
+++ projects/catalog-fm-app/src/app/last-fm/scrobble/manual-scrobble/manual-scrobble.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ManualScrobbleRoutingModule } from './manual-scrobble-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ManualScrobbleRoutingModule
+  ]
+})
+export class ManualScrobbleModule { }

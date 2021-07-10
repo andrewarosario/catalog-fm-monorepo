06f23b0756b92b9e45a9f2038b7727e9eeba9f0a
Andrew Rosário <andrew.arosario@gmail.com>
2021-07-09 23:10:11 -0300

    test: ensure ManualScrobblePageComponent form is valid only when typing required fields

--- projects/catalog-fm-app/src/app/last-fm/scrobble/manual-scrobble/pages/manual-scrobble-page/manual-scrobble-page.component.spec.ts
+++ projects/catalog-fm-app/src/app/last-fm/scrobble/manual-scrobble/pages/manual-scrobble-page/manual-scrobble-page.component.spec.ts
@@ -1,25 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { render, screen } from '@testing-library/angular';
+import userEvent from '@testing-library/user-event';
 import { ManualScrobblePageComponent } from './manual-scrobble-page.component';
 
+const getSubmitButton = () => screen.getByTestId('submit') as HTMLButtonElement;
+const setup = async () => {
+  await render(ManualScrobblePageComponent, {
+    imports: [ReactiveFormsModule],
+  });
+};
+
 describe('ManualScrobblePageComponent', () => {
-  let component: ManualScrobblePageComponent;
-  let fixture: ComponentFixture<ManualScrobblePageComponent>;
+  it('form should be valid only when typing required fields', async () => {
+    await setup();
+    expect(getSubmitButton().disabled).toBe(true);
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ManualScrobblePageComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ManualScrobblePageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    userEvent.type(screen.getByTestId('artist'), 'artist');
+    userEvent.type(screen.getByTestId('track'), 'track');
+    expect(getSubmitButton().disabled).toBe(false);
   });
 });

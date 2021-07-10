42843221e8599b357e106ad894d6586d9a1a18cb
Andrew Rosário <andrew.arosario@gmail.com>
2021-07-09 23:55:46 -0300

    test: inject scrobbleService in ManualScrobblePageComponent

--- projects/catalog-fm-app/src/app/last-fm/scrobble/manual-scrobble/pages/manual-scrobble-page/manual-scrobble-page.component.spec.ts
+++ projects/catalog-fm-app/src/app/last-fm/scrobble/manual-scrobble/pages/manual-scrobble-page/manual-scrobble-page.component.spec.ts
@@ -1,3 +1,5 @@
+import { ScrobbleService } from '@/last-fm/services/scrobble/scrobble.service';
+import { makeScrobbleService } from '@/last-fm/services/scrobble/scrobble.service.mock';
 import { ReactiveFormsModule } from '@angular/forms';
 import { render, screen } from '@testing-library/angular';
 import userEvent from '@testing-library/user-event';
@@ -5,18 +7,27 @@ import { ManualScrobblePageComponent } from './manual-scrobble-page.component';
 
 const getSubmitButton = () => screen.getByTestId('submit') as HTMLButtonElement;
 const setup = async () => {
+  const scrobbleServiceSpy = makeScrobbleService();
   await render(ManualScrobblePageComponent, {
     imports: [ReactiveFormsModule],
+    providers: [{ provide: ScrobbleService, useValue: scrobbleServiceSpy }],
   });
+  return { scrobbleServiceSpy };
 };
 
 describe('ManualScrobblePageComponent', () => {
   it('form should be valid only when typing required fields', async () => {
-    await setup();
+    const { scrobbleServiceSpy } = await setup();
     expect(getSubmitButton().disabled).toBe(true);
 
     userEvent.type(screen.getByTestId('artist'), 'artist');
+    expect(getSubmitButton().disabled).toBe(true);
+
     userEvent.type(screen.getByTestId('track'), 'track');
     expect(getSubmitButton().disabled).toBe(false);
+
+    userEvent.clear(screen.getByTestId('artist'));
+    userEvent.click(getSubmitButton());
+    expect(scrobbleServiceSpy.scrobble).not.toHaveBeenCalled();
   });
 });

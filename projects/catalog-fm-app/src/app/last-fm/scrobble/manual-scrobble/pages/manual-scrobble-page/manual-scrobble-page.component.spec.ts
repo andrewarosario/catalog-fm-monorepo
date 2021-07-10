cb244e3a90a22917064dc6fde09e1a54567b05a9
Andrew Rosário <andrew.arosario@gmail.com>
2021-07-10 00:11:51 -0300

    test: call scrobble service on manual scrobble

--- projects/catalog-fm-app/src/app/last-fm/scrobble/manual-scrobble/pages/manual-scrobble-page/manual-scrobble-page.component.spec.ts
+++ projects/catalog-fm-app/src/app/last-fm/scrobble/manual-scrobble/pages/manual-scrobble-page/manual-scrobble-page.component.spec.ts
@@ -3,6 +3,7 @@ import { makeScrobbleService } from '@/last-fm/services/scrobble/scrobble.servic
 import { ReactiveFormsModule } from '@angular/forms';
 import { render, screen } from '@testing-library/angular';
 import userEvent from '@testing-library/user-event';
+import { MOCK_LAST_FM_SIMPLE_TRACK } from 'last-fm';
 import { ManualScrobblePageComponent } from './manual-scrobble-page.component';
 
 const getSubmitButton = () => screen.getByTestId('submit') as HTMLButtonElement;
@@ -20,14 +21,23 @@ describe('ManualScrobblePageComponent', () => {
     const { scrobbleServiceSpy } = await setup();
     expect(getSubmitButton().disabled).toBe(true);
 
-    userEvent.type(screen.getByTestId('artist'), 'artist');
+    userEvent.type(screen.getByTestId('artist'), MOCK_LAST_FM_SIMPLE_TRACK.artist);
     expect(getSubmitButton().disabled).toBe(true);
 
-    userEvent.type(screen.getByTestId('track'), 'track');
+    userEvent.type(screen.getByTestId('track'), MOCK_LAST_FM_SIMPLE_TRACK.track);
     expect(getSubmitButton().disabled).toBe(false);
 
     userEvent.clear(screen.getByTestId('artist'));
     userEvent.click(getSubmitButton());
     expect(scrobbleServiceSpy.scrobble).not.toHaveBeenCalled();
+  });
+
+  it('should scrobble track', async () => {
+    const { scrobbleServiceSpy } = await setup();
+    userEvent.type(screen.getByTestId('artist'), MOCK_LAST_FM_SIMPLE_TRACK.artist);
+    userEvent.type(screen.getByTestId('track'), MOCK_LAST_FM_SIMPLE_TRACK.track);
+    userEvent.type(screen.getByTestId('album'), MOCK_LAST_FM_SIMPLE_TRACK.album);
+    userEvent.click(getSubmitButton());
+    expect(scrobbleServiceSpy.scrobble).toHaveBeenCalledWith(MOCK_LAST_FM_SIMPLE_TRACK);
   });
 });

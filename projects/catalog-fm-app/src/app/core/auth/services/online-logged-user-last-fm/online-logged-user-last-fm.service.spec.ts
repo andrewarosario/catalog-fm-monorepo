c8901aa2370a9d1ebe4b31d3c2be809f4f3d43ec
Andrew Rosário <andrew.arosario@gmail.com>
2021-07-10 22:31:43 -0300

    test: OnlineLoggedUserLastFmService cases

--- projects/catalog-fm-app/src/app/core/auth/services/online-logged-user-last-fm/online-logged-user-last-fm.service.spec.ts
+++ projects/catalog-fm-app/src/app/core/auth/services/online-logged-user-last-fm/online-logged-user-last-fm.service.spec.ts
@@ -1,4 +1,6 @@
-import { BehaviorSubject } from 'rxjs';
+import { spyPropertyGetter } from 'projects/catalog-fm-app/src/test/helpers/jasmine.spec-helpers';
+import { BehaviorSubject, of } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { makeAuthUserStore } from '../../store/auth-user.store.mock';
 import { OnlineLoggedUserLastFmService } from './online-logged-user-last-fm.service';
 
@@ -13,5 +15,50 @@ describe('OnlineLoggedUserLastFmService', () => {
   it('should be created', () => {
     const { service } = makeSut();
     expect(service).toBeTruthy();
+  });
+
+  it('should return false when not logged and offline', () => {
+    const { service, authUserStoreSpy, isOfflineMock } = makeSut();
+    isOfflineMock.next(true);
+    spyPropertyGetter(authUserStoreSpy, 'authUser$').and.returnValue(of(null));
+    service
+      .isOnlineAndLogged()
+      .pipe(take(1))
+      .subscribe((isOnlineAndLogged) => {
+        expect(isOnlineAndLogged).toBe(false);
+      });
+  });
+
+  it('should return false when not logged and online', () => {
+    const { service, authUserStoreSpy } = makeSut();
+    spyPropertyGetter(authUserStoreSpy, 'authUser$').and.returnValue(of(null));
+    service
+      .isOnlineAndLogged()
+      .pipe(take(1))
+      .subscribe((isOnlineAndLogged) => {
+        expect(isOnlineAndLogged).toBe(false);
+      });
+  });
+
+  it('should return false when logged and offline', () => {
+    const { service, isOfflineMock } = makeSut();
+    isOfflineMock.next(true);
+
+    service
+      .isOnlineAndLogged()
+      .pipe(take(1))
+      .subscribe((isOnlineAndLogged) => {
+        expect(isOnlineAndLogged).toBe(false);
+      });
+  });
+
+  it('should return true when logged and online', () => {
+    const { service } = makeSut();
+    service
+      .isOnlineAndLogged()
+      .pipe(take(1))
+      .subscribe((isOnlineAndLogged) => {
+        expect(isOnlineAndLogged).toBe(true);
+      });
   });
 });

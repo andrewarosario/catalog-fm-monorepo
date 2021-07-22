b723464c5ca727c2330017a36daf381622e87a4d
andrewarosario <andrew.arosario@gmail.com>
2021-07-22 20:38:40 -0300

    feat: create StartupService

---
+++ projects/catalog-fm-app/src/app/core/services/startup/startup.service.spec.ts
@@ -0,0 +1,31 @@
+import { ScrobbleSynchronizationService } from '@/last-fm/scrobble/services/scrobble-online-sync/scrobble-synchronization.service';
+import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+
+import { StartupService } from './startup.service';
+
+const makeScrobbleSynchronizationServiceSpy =
+  (): jasmine.SpyObj<ScrobbleSynchronizationService> => {
+    return jasmine.createSpyObj<ScrobbleSynchronizationService>('ScrobbleSynchronizationService', {
+      synchronizeScrobbles: of(),
+    });
+  };
+
+const makeSut = () => {
+  const scrobbleSynchronizationServiceSpy = makeScrobbleSynchronizationServiceSpy();
+  const service = new StartupService(scrobbleSynchronizationServiceSpy);
+  return { service, scrobbleSynchronizationServiceSpy };
+};
+
+describe('StartupService', () => {
+  it('should be created', () => {
+    const { service } = makeSut();
+    expect(service).toBeTruthy();
+  });
+
+  it('should call scrobbleSynchronizationService.synchronizeScrobbles on load', () => {
+    const { service, scrobbleSynchronizationServiceSpy } = makeSut();
+    service.load();
+    expect(scrobbleSynchronizationServiceSpy.synchronizeScrobbles).toHaveBeenCalled();
+  });
+});
---
+++ projects/catalog-fm-app/src/app/core/services/startup/startup.service.ts
@@ -0,0 +1,13 @@
+import { ScrobbleSynchronizationService } from '@/last-fm/scrobble/services/scrobble-online-sync/scrobble-synchronization.service';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StartupService {
+  constructor(private scrobbleSynchronizationService: ScrobbleSynchronizationService) {}
+
+  load(): void {
+    this.scrobbleSynchronizationService.synchronizeScrobbles().subscribe();
+  }
+}

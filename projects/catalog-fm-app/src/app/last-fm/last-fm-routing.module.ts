4f6fc86754d7bdc2914640504788e48e3ad8481a
andrewarosario <andrew.arosario@gmail.com>
2021-07-23 15:32:58 -0300

    feat: remove LoggedUserLastFmGuard to scrobble routes

--- projects/catalog-fm-app/src/app/last-fm/last-fm-routing.module.ts
+++ projects/catalog-fm-app/src/app/last-fm/last-fm-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoggedUserLastFmGuard } from './guards/logged-user-last-fm/logged-user-last-fm.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'scrobble', pathMatch: 'full' },
   {
     path: 'scrobble',
-    canActivate: [LoggedUserLastFmGuard],
     loadChildren: () => import('./scrobble/scrobble.module').then((m) => m.ScrobbleModule),
   },
 ];

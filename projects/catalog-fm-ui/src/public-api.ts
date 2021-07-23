a4f8e7f86ca2cdcf173c3714df3f81e8118c0424
andrewarosario <andrew.arosario@gmail.com>
2021-07-23 16:11:57 -0300

    feat: delete default ui-lib files

--- projects/catalog-fm-ui/src/public-api.ts
+++ projects/catalog-fm-ui/src/public-api.ts
@@ -1,7 +1,3 @@
 /*
  * Public API Surface of catalog-fm-ui
  */
-
-export * from './lib/catalog-fm-ui.service';
-export * from './lib/catalog-fm-ui.component';
-export * from './lib/catalog-fm-ui.module';

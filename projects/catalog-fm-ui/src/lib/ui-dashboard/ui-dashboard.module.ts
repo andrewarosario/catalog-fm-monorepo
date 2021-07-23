0988f1fc77edc7e1d460397799dbac0cb294bf44
andrewarosario <andrew.arosario@gmail.com>
2021-07-23 16:31:41 -0300

    feat: create dashboard module

---
+++ projects/catalog-fm-ui/src/lib/ui-dashboard/ui-dashboard.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { IconsModule } from '../ng-zorro/icons.module';
+
+registerLocaleData(en);
+
+@NgModule({
+  declarations: [DashboardComponent],
+  imports: [
+    CommonModule,
+    NzLayoutModule,
+    NzMenuModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    IconsModule
+  ],
+  exports: [DashboardComponent],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
+})
+export class UiDashboardModule { }
--- projects/catalog-fm-ui/src/public-api.ts
+++ projects/catalog-fm-ui/src/public-api.ts
@@ -1,3 +1,6 @@
 /*
  * Public API Surface of catalog-fm-ui
  */
+export * from './lib/ng-zorro/icons.module'
+export * from './lib/ui-dashboard/ui-dashboard.module';
+export * from './lib/ui-dashboard/dashboard/dashboard.component';

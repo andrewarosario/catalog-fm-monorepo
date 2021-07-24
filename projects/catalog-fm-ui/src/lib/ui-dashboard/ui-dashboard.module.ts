faaeda50bb578e42273940b801999b68ae4aee62
andrewarosario <andrew.arosario@gmail.com>
2021-07-23 22:47:53 -0300

    refactor: improves readability to DashboardHeaderComponent

--- projects/catalog-fm-ui/src/lib/ui-dashboard/ui-dashboard.module.ts
+++ projects/catalog-fm-ui/src/lib/ui-dashboard/ui-dashboard.module.ts
@@ -12,13 +12,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { IconsModule } from '../ng-zorro/icons.module';
 import { DashboardContentComponent } from './dashboard-content/dashboard-content.component';
-import { DashboardSiderComponent } from './dashboard-sider/dashboard-sider.component';
 import { DashboardHeaderComponent } from './dashboard-header/dashboard-header.component';
 
 registerLocaleData(en);
 
 @NgModule({
-  declarations: [DashboardComponent, DashboardContentComponent, DashboardSiderComponent, DashboardHeaderComponent],
+  declarations: [DashboardComponent, DashboardContentComponent, DashboardHeaderComponent],
   imports: [
     CommonModule,
     NzLayoutModule,

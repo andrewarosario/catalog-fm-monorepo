4771278d4dab48492b6ce1ed651fe65069deacc1
andrewarosario <andrew.arosario@gmail.com>
2021-07-27 09:37:30 -0300

    feat: apply disable on progress on manual scrobble

--- projects/catalog-fm-app/src/app/last-fm/scrobble/manual-scrobble/manual-scrobble.module.ts
+++ projects/catalog-fm-app/src/app/last-fm/scrobble/manual-scrobble/manual-scrobble.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { ManualScrobbleRoutingModule } from './manual-scrobble-routing.module';
 import { ManualScrobblePageComponent } from './pages/manual-scrobble-page/manual-scrobble-page.component';
-import { UiIconModule, UiFormModule } from 'catalog-fm-ui';
+import { UiIconModule, UiFormModule, UiProgressModule } from 'catalog-fm-ui';
 
 @NgModule({
   declarations: [ManualScrobblePageComponent],
-  imports: [CommonModule, UiFormModule, ManualScrobbleRoutingModule, UiIconModule],
+  imports: [
+    CommonModule,
+    UiFormModule,
+    ManualScrobbleRoutingModule,
+    UiIconModule,
+    UiProgressModule,
+  ],
 })
 export class ManualScrobbleModule {}
